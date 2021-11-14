8edd64889b0e86cae21b52038cf888c5e451639c
RubelBiswasCS <rubelbiswas36@gmail.com>
2021-11-14 20:41:47 +0600

    modified utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,9 @@
 
 const shortenStr = (str, n) => {
     var cut = str.indexOf(' ', n);
-    if (cut == -1) return str;
+    if (cut == -1){
+        return str;
+        }
     return str.substring(0, cut)
 }
 
