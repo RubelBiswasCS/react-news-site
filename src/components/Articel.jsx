21ce8569f3cfa6c0de8c0024c81d78dce7a8a184
RubelBiswasCS <rubelbiswas36@gmail.com>
2021-11-16 13:57:00 +0600

    modified article

--- src/components/Articel.jsx
+++ src/components/Articel.jsx
@@ -7,10 +7,19 @@ import {
 
   const Article = (props) => {
         return (
-            <div style={{backgroundColor:"red",minWidth:"10px",minHeight:"10px"}}>
+            <div className={"article"}>
                 <h1 className="title">
                     {props.article.title}
                 </h1>
+                <small>{props.article.published_at.slice(0,10)}</small>
+                <p>{props.article.description}</p>
+                <img src={props.article.image} alt=''/>
+                <span>Author: {props.article.author}</span>
+                
+                <p>
+                    {props.article.description}
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam distinctio perspiciatis, iure earum molestias omnis, at similique nostrum cumque ea et? Laudantium, repellendus. Accusamus quos quis praesentium dicta est soluta. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, consectetur dignissimos a officiis sunt aperiam iusto quia adipisci dolore ipsa harum praesentium magni, possimus nobis fugit et commodi itaque amet!
+                </p>
             </div>
         );
     };
